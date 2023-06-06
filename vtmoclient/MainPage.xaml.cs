28ef2bbbf849e94550ff122005c6b8315f72e371
antondudarenkov <adudarenkov@gmail.com>
2023-06-06 22:49:15 +0300

    init

---
+++ vtmoclient/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http.Json;
+using Resource = Microsoft.Maui.Controls;
+
+namespace vtmoclient;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    record class User(int id, string name, string password, bool canBeGm);
+    private static HttpClient sharedClient = new()
+    {
+        BaseAddress = new Uri("http://localhost:8080"),
+    };
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+    
+        // count++;
+        //
+        // if (count == 1)
+        //     CounterBtn.Text = $"Clicked {count} time";
+        // else
+        //     CounterBtn.Text = $"Clicked {count} times";
+        var user = await sharedClient.GetFromJsonAsync<User>("/user/1");
+        CounterBtn.Text = user.name;
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
